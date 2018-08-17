e237c83bec83f2adc3bd5e80ad8eb8b19b1ddf7f
Ersh1237 <36787400+Ersh1237@users.noreply.github.com>
2018-08-17 20:28:22 +0300

    second change

--- fun1.js
+++ fun1.js
@@ -3,10 +3,10 @@
 var saveFun = function() {
     var obj={};
     localStorage.setItem('title',document.getElementById('12').value);
-    localStorage.setItem('author',document.getElementById('123').value);
+        localStorage.setItem('author',document.getElementById('123').value);
     localStorage.setItem('name',document.getElementById('124').value);
-    obj.tit=localStorage.getItem('title');
-    obj.author=localStorage.getItem('author');
+         obj.tit=localStorage.getItem('title');
+       obj.author=localStorage.getItem('author');
     obj.name=localStorage.getItem('name');
     console.log(obj);
     localStorage.setItem('store',JSON.stringify(obj));
